a24b806e842c7788b6a7d6fbc05724226eba6ec3
RohanPhadatare <43084002+RohanPhadatare@users.noreply.github.com>
2018-10-28 20:06:40 +0530

    Update signedup.php

--- signedup.php
+++ signedup.php
@@ -6,20 +6,19 @@ include 'conn.php';
 	$pass1=$_POST['pass'];
 	$mobile1=$_POST['mobile'];
 	$flag=0;
-			$select=mysqli_query($conn,"SELECT * from user where email='".$email1."'");
-        	
-        	while($result=mysqli_fetch_array($select)){
-            	$flag=$flag+1;
-        	}
-        	if($flag==0){
-            	$insert = $conn->query("INSERT INTO tk.user (name,email,password,mobile) VALUES ('".$name1."','".$email1."','".$pass1."','".$mobile1."')");
-            	echo "<script type=\"text/javascript\">window.alert('New record inserted successfully!');
-				window.location.href = 'signin.html';</script>"; 
-				exit;
-        	}else{
-				echo "<script type=\"text/javascript\">window.alert('Someone already registered using this email!');
-				window.location.href = 'signup.html';</script>"; 
-				exit;
-    	    }
-
+		$select=mysqli_query($conn,"SELECT * from user where email='".$email1."'");
+	
+    	while($result=mysqli_fetch_array($select)){
+	    	$flag=1;
+       	}
+    	if($flag==0){
+        	$insert = $conn->query("INSERT into user (name,email,password,mobile) VALUES ('".$name1."','".$email1."','".$pass1."','".$mobile1."')");
+			echo "<script type=\"text/javascript\">window.alert('New record inserted successfully!');
+			window.location.href = 'signin.html';</script>"; 
+			exit;
+    	}else{
+			echo "<script type=\"text/javascript\">window.alert('Someone already registered using this email!');
+			window.location.href = 'signup.html';</script>"; 
+			exit;
+	    }
 ?>
