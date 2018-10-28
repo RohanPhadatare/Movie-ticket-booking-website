0df1ab5877e19ed56b2719f465b1a45d326cbea9
RohanPhadatare <43084002+RohanPhadatare@users.noreply.github.com>
2018-10-28 19:55:30 +0530

    Update signedin.php

--- signedin.php
+++ signedin.php
@@ -4,20 +4,21 @@ session_start();
 	$email1=$_POST['email'];
 	$pass1=$_POST['pass'];
 
-	$select=mysqli_query($conn,"SELECT * from `user` where email='".$email1."'");
-	while($result=mysqli_fetch_array($select)){
-		$mail=$result['email'];
-		$pwd=$result['password'];
-		$name=$result['name'];
-		if($mail!=$email1 || $pwd!=$pass1){
-			echo "<script type=\"text/javascript\">window.alert('Incorrect email or password!');
-			window.location.href = 'signin.html';</script>"; 
-			exit;
-		}else{
-			$_SESSION['nm']=$name;
-			echo "<script type=\"text/javascript\">window.alert('Logged in successfully!');
-			window.location.href = 'user.php';</script>"; 
-			exit;
-		}
+	$select=mysqli_query($conn,"SELECT * from `user` where email='".$email1."' AND password='".$pass1."'");
+	$flag=0;
+	while($row=mysqli_fetch_array($select)){
+		$flag=1;
+		$name=$row['name'];
 	}
+	if($flag==1){
+		$_SESSION['nm']=$name;
+		echo "<script type=\"text/javascript\">window.alert('Logged in successfully!');
+		window.location.href = 'user.php';</script>"; 
+		exit;
+	}else{
+		echo "<script type=\"text/javascript\">window.alert('Incorrect email or password!');
+		window.location.href = 'signin.html';</script>"; 
+		exit;
+	}
+
 ?>
